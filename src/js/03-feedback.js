05231e2858f511ead6feb06a41f8bde22b66d28a
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-03-19 18:59:22 +0200

    add result task_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,46 +1,29 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-  email: document.querySelector('.feedback-form input'),
-};
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
 
-const STORAGE_KEY = 'feedback-msg';
 const formData = {};
 
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
-refs.email.addEventListener('input', onEmailInput);
-
-populateTexterea();
-
-function onEmailInput(event) {
-  formData[event.target.email] = event.target.value;
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-function onFormSubmit(event) {
-  event.preventDefault();
-  event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
   localStorage.removeItem('feedback-form-state');
 }
 
-function onTextareaInput(evt) {
-  const message = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, message);
-}
-
-function populateTexterea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  const saveEmail = localStorage.getItem('feedback-form-state');
-  const parsetEmail = JSON.parse(saveEmail);
-  if (savedMessage && parsetEmail) {
-    console.log(savedMessage);
-    refs.textarea.value = savedMessage;
-    console.log(parsetEmail);
-    refs.email.value = saveEmail;
+(function dataFromLocalStorage() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
   }
-}
-console.log(localStorage);
+})();
